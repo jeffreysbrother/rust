bc6e8fd57ae6c481bd2dba488ed961970a6096ab
James Cool <jeffreysbrother@gmail.com>
2015-11-13 17:56:43 -0800

    primitive types

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,27 @@ fn main() {
 
     print_sum(20, 50);
 
+
+    // primitive types #############################
+
+    //bool
+    let ted: bool = true;
+    print!("the value of ted is: {}\n", ted);
+
+    //char...these are created with single ticks ''
+    let david = 'x';
+    print!("the value of david is: {}\n", david);
+
+    //a lot of numeric types I won't list here
+
+    //arrays
+    let aaron = [1, 2, 3];
+    let anna = [4, 5, 6, 12, 56, 123];
+
+    print!("aaron has {} elements\n", aaron.len());
+    print!("anna has {} elements\n", anna.len());
+
+
 }
 
 fn print_sum(x: i32, y: i32) {
