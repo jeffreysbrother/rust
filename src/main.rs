fb1eb1a876b62a98ae04997b6520316e482435be
James Cool <jeffreysbrother@gmail.com>
2015-11-13 16:14:20 -0800

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 fn main() {
 
-    let x = 25;
+    let number1 = 25;
+    println!("The value of number1 is: {}", number1);
 
-    println!("The value of x is: {}", x);
+    let number2: i32 = 100;
+    print!("The value of number2 is {}\n", number2);
 
+    print_sum(20, 50);
 
+}
+
+fn print_sum(x: i32, y: i32) {
+    print!("the sum of the two values provided is: {}", x + y);
 }
